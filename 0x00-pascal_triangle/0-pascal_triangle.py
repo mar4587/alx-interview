6574d656f7d474eb98e9ecc4c57d26908c46c0da
mar4587 <marshetnigatu@gmail.com>
2024-07-25 01:27:07 -0700

    Ps triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,12 @@
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    triangle = [[1]]
+    for i in range(1, n):
+        previous = triangle[-1]
+        current = [1]
+        for j in range(1, i):
+            current.append(previous[j-1] + previous[j])
+        current.append(1)
+        triangle.append(current)
+    return triangle
